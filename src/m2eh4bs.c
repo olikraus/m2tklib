2e9f1c818e12eb2b41a62122fd00b966ec4a4bd6
olikraus <olikraus@gmail.com>
2013-01-05 11:22:26 +0100

    issue 92

--- src/m2eh4bs.c
+++ src/m2eh4bs.c
@@ -52,19 +52,19 @@ uint8_t m2_eh_4bs(m2_p ep, uint8_t msg, uint8_t arg)
       
       /* if the field has data entry, than increment the data */
       if ( m2_nav_is_data_entry(nav) )
-	return m2_nav_data_up(m2_get_nav(ep));
+	return m2_nav_data_up(nav);
       
       /* else go down: do not use m2_nav_down, instead use m2_nav_user_down to skip design elements */
       return m2_nav_user_down(nav, 1);
 
     case M2_EP_MSG_EXIT:
-      return m2_nav_user_up(m2_get_nav(ep));
+      return m2_nav_user_up(nav);
       
     case M2_EP_MSG_NEXT:
-      return m2_nav_user_next(m2_get_nav(ep));
+      return m2_nav_user_next(nav);
 
     case M2_EP_MSG_PREV:
-      return m2_nav_user_prev(m2_get_nav(ep));
+      return m2_nav_user_prev(nav);
 
   }
   return 0;
