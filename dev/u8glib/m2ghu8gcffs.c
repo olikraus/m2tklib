b7355ab312ec3ccfdcfbe4ce04f47fc6c2958281
olikraus <olikraus@gmail.com>
2013-07-09 18:19:11 +0200

    color style m2

--- dev/u8glib/m2ghu8gcffs.c
+++ dev/u8glib/m2ghu8gcffs.c
@@ -28,7 +28,9 @@
 
 
 uint8_t m2_u8g_frame_shadow_color = 0x02;
-uint8_t m2_u8g_frame_color =  0x27;	
+uint8_t m2_u8g_frame_color =  0x27;
+uint8_t m2_u8g_background_color = 0x0e0 | 0x010 | 0x000;
+uint8_t m2_u8g_normal_field_focus_color = 0x000 | 0x010 | 0x003;
 
 void m2_u8g_draw_color_frame_shadow(uint8_t x0, uint8_t y0, uint8_t w, uint8_t h)
 {
@@ -46,6 +48,21 @@ void m2_u8g_draw_color_frame_shadow(uint8_t x0, uint8_t y0, uint8_t w, uint8_t h
   u8g_DrawHLine(m2_u8g, x0+1, y+h, w);
 }
 
+/* origin is low left */
+void m2_u8g_draw_color_box(uint8_t x0, uint8_t y0, uint8_t w, uint8_t h, uint8_t color)
+{
+  u8g_uint_t y;
+  /* transform y to upper left */
+  y = m2_u8g_height_minus_one;
+  y -= y0;
+  y -= h;
+  y++;			/* 13 Jan 2013: Issue 95 */
+  //u8g_SetColorIndex(m2_u8g, m2_u8g_draw_color);
+  u8g_SetColorIndex(m2_u8g, color);
+  u8g_DrawBox(m2_u8g, x0, y, w, h);
+}
+
+
 
 /*
   color version
@@ -59,6 +76,7 @@ uint8_t m2_gh_u8g_cffs(m2_gfx_arg_p  arg)
   switch(arg->msg)
   {
     case M2_GFX_MSG_DRAW_NORMAL_NO_FOCUS:
+      //m2_u8g_draw_color_box(arg->x+1, arg->y+1, arg->w-2, arg->h-2, m2_u8g_normal_field_focus_color);
       if ( (arg->font & 4) != 0 )
       {
         /* highlight flag is set, draw frame with shadow */
@@ -80,8 +98,9 @@ uint8_t m2_gh_u8g_cffs(m2_gfx_arg_p  arg)
             highlight version
             draw frame with shadow together with a filled box 
         */
+	
         m2_u8g_current_text_color = m2_u8g_bg_text_color;
-        m2_u8g_draw_box(arg->x+1, arg->y+1, arg->w-2, arg->h-2);
+        m2_u8g_draw_color_box(arg->x+1, arg->y+1, arg->w-2, arg->h-2, m2_u8g_fg_text_color);
       	m2_u8g_draw_color_frame_shadow(arg->x+m2_gh_u8g_invisible_frame_border_x_size, arg->y, arg->w-2*m2_gh_u8g_invisible_frame_border_x_size, arg->h);
         m2_gh_u8g_invert_at_depth = m2_gh_u8g_current_depth;
       }
@@ -98,13 +117,13 @@ uint8_t m2_gh_u8g_cffs(m2_gfx_arg_p  arg)
       break;
     case M2_GFX_MSG_DRAW_SMALL_FOCUS:
       m2_u8g_current_text_color = m2_u8g_bg_text_color;
-      m2_u8g_draw_box(arg->x, arg->y, arg->w, arg->h);
+      m2_u8g_draw_color_box(arg->x, arg->y, arg->w, arg->h, m2_u8g_fg_text_color);
       break;
     case M2_GFX_MSG_DRAW_GO_UP:
       /* does not work because of missing xor...
          also: can not be fixed with FRAME_DRAW_AT_END
        m2_u8g_current_text_color = m2_u8g_bg_text_color; */
-      m2_u8g_draw_box(arg->x, arg->y, arg->w, arg->h/2);
+      m2_u8g_draw_color_box(arg->x, arg->y, arg->w, arg->h/2, m2_u8g_fg_text_color);
       break;
     case M2_GFX_MSG_GET_NORMAL_BORDER_HEIGHT:
       return 3;
@@ -120,6 +139,10 @@ uint8_t m2_gh_u8g_cffs(m2_gfx_arg_p  arg)
       return 0;
     case M2_GFX_MSG_IS_FRAME_DRAW_AT_END:
       return 0; /* focus (highlight) is drawn first, then the text string */
+    case M2_GFX_MSG_START:
+      u8g_SetColorIndex(m2_u8g, m2_u8g_background_color);
+      u8g_DrawBox(m2_u8g, 0, 0, u8g_GetWidth(m2_u8g), u8g_GetHeight(m2_u8g));
+      break;
   }
 
   return m2_gh_u8g_base(arg);
